beded5db94dafdd410828030cb79a7d6cbebe310
madya santosa <devmadya@gmail.com>
2023-10-30 18:54:09 +0800

    [fix] fix bug registration token not insert

--- features/users/repository/update_user_repository.go
+++ features/users/repository/update_user_repository.go
@@ -7,7 +7,7 @@ import (
 
 func (r *UserRepositoryImpl) UpdateUser(user *model.User, username string) (*model.User, error) {
 	users := model.User{}
-	result := r.DB.Model(&users).Where("username = ?", username).Updates(model.User{FullName: user.FullName, Username: user.Username, StudyProgram: user.StudyProgram, Phone: user.Phone})
+	result := r.DB.Model(&users).Where("username = ?", username).Updates(model.User{FullName: user.FullName, Username: user.Username, StudyProgram: user.StudyProgram, Phone: user.Phone, RegistrationToken: user.RegistrationToken})
 	fmt.Println(result)
 	if result.Error != nil {
 		return nil, result.Error
